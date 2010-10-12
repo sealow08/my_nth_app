d43652cc84028f18073343a085ed01dec9f80c2c
sealow08 <sealow08@yahoo.com>
2010-10-12 18:25:23 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyNthApp::Application.config.secret_token = '7ebd0e950e401ac00be2d58286498f84f940d34721ad744eb0507d9b4c1dfb3f2f2a05e6d15d5329d09a9c7c62b866518527a9ab066de05c9824f986853a662f'
